7979a59f9ba9b322023b1ebe43b147a138967289
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-23 08:52:22 +0200

    Tilføjet antal af forsøg, der udskrives til brugeren , når der er gættet rigtigt

--- GuessNumber/Program.cs
+++ GuessNumber/Program.cs
@@ -13,11 +13,12 @@ namespace GuessNumber
 
             int numberToGuess = random.Next(1, 100);
             int guess = 0;
+            int guesses = 0;
 
             Console.WriteLine("Hvad gætter du på tallet er?");
             while (guess != numberToGuess)
             {
-
+                guesses++;
                 guess = Convert.ToInt16(Console.ReadLine());
 
                 if (guess > numberToGuess)
@@ -31,7 +32,8 @@ namespace GuessNumber
                 } else if (guess == numberToGuess)
                 {
                     Console.ForegroundColor = ConsoleColor.Cyan;
-                    Console.WriteLine("TIllykke, du har gættede det rigtige tal.");
+                    Console.WriteLine("Tillykke, du har gættede det rigtige tal.");
+                    Console.WriteLine("Du brugte {0} forsøg på at gætte rigtigt", guesses);
                 }
             }
         }
