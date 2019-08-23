60f5dd1bb740959c4155b522fe979e73009d4bb6
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-08-23 08:26:05 +0200

    Add project files.

---
+++ GuessNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberToGuess = 10;
+            int guess = 0;
+
+            Console.WriteLine("Hvad gætter du på tallet er?");
+            while (guess != numberToGuess)
+            {
+
+                guess = Convert.ToInt16(Console.ReadLine());
+
+                if (guess > numberToGuess)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Dit gæt på {0} er for højt, prøv igen", guess);
+                } else if (guess < numberToGuess)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Dit gæt på {0} er for lavt, prøv igen", guess);
+                } else if (guess == numberToGuess)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("TIllykke, du har gættede det rigtige tal.");
+                }
+            }
+        }
+    }
+}
